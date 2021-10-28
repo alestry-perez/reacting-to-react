65cecf84db58280526d8f74cda7d49a5c2e279a1
Alestry Pérez <alestry.perez@hyperisland.se>
2021-10-28 11:17:43 +0200

    init branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-import Contact from './components/Contact';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import WeatherPage from './components/WeatherPage';
-import HomePage from './components/HomePage';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/home" component={HomePage} />
-        <Route exact path="/current_weather" component={WeatherPage} />
-        <Route exact path="/location_weather" component={WeatherPage} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
-    </Router>
+    <div>
+      <h1>HELLO WORLD</h1>
+    </div>
   );
 }
 
