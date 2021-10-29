98802d29aeee87a8a83acf91588e97065032e24b
Alestry Pérez <alestry.perez@hyperisland.se>
2021-10-29 10:58:41 +0200

    Init project files
    Co-authored-by: EphraimVC <EphraimVC@users.noreply.github.com>

--- src/components/WeatherPage.tsx
+++ src/components/WeatherPage.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const WeatherPage = ({ weatherData }) => {
-  return (
-    <div>
-      <h1>{weatherData.name}</h1>
-    </div>
-  );
+const WeatherPage = () => {
+  return <div></div>;
 };
 
 export default WeatherPage;
---
+++ src/components/WeatherPanel.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherPanel = () => {
+  return <div></div>;
+};
+
+export default WeatherPanel;
