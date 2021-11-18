7d87e1d4f20b1963b4fc7488942996bb8e168ba4
Alestry Pérez <alestry.perez@hyperisland.se>
2021-11-18 12:29:33 +0100

    Cypress Implementation

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,30 +1,32 @@
 describe('renders the home page', () => {
-  it('navbar renders', () => {
-    cy.visit('/');
-    cy.get('#navbar').should('exist');
-  });
-  it('navbar links can be clicked', () => {
-    cy.visit('/');
+  // beforeEach(() => {
+  //   cy.visit('/');
+  // });
 
-    cy.get('[href="/home"] > li').click();
-    cy.get('[href="/about"] > li').click();
-    cy.get('[href="/local/weather"] > li').click();
-    cy.get('[href="/foreign/weather?lat=12.0464&lon=77.0428"] > li').click();
+  it('user log in', () => {
+    cy.visit('/');
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-username'
+    ).clear();
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-username'
+    ).type('alestry_perez');
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-password'
+    ).click();
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-password'
+    ).clear();
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-password'
+    ).type('eefcx0jr');
+    cy.get(
+      '#page_x002e_components_x002e_slingshot-login_x0023_default-submit-button'
+    ).click();
   });
-  it('Content should exist', () => {
-    cy.get('h1').should('be.visible');
-    cy.get('#root > :nth-child(2) > :nth-child(2)').should('be.visible');
-    cy.get('#root > :nth-child(2) > :nth-child(3)').should('be.visible');
-  });
-  it('Find content', () => {
-    cy.get('h1').should('have.text', 'Todays Weather in:');
-    cy.get('#root > :nth-child(2) > :nth-child(2)').should(
-      'have.text',
-      'Local Temperature is:'
-    );
-    cy.get('#root > :nth-child(2) > :nth-child(3)').should(
-      'have.text',
-      'Current Conditions are:'
-    );
+  // clicks on interns demo link
+  it('clicks on interns demo link', () => {
+    cy.get('body').should('exist');
+    cy.url().should('include', '/dashboard');
   });
 });
